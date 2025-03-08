dddc3938432d30fa6d17833701b85a9bb5aade68
RB Metcalf <bmetcalf@gmail.com>
2025-03-08 14:36:56 +0100

    Compatibility updates.

--- testparticles.cpp
+++ testparticles.cpp
@@ -77,8 +77,8 @@ int main(int arg,char **argv){
   lens.ResetSourcePlane(z_source,false);
   
   // output some maps
-  gridmap.writeFits(LensingVariable::KAPPA,"!particles_kappa.fits");
-  gridmap.writeFits(LensingVariable::INVMAG,"!particles_invmag.fits");
+  gridmap.writeFits<float>(LensingVariable::KAPPA,"!particles_kappa.fits");
+  gridmap.writeFits<float>(LensingVariable::INVMAG,"!particles_invmag.fits");
   
   // find the critical curves
   std::vector<ImageFinding::CriticalCurve> crit_curve;
@@ -101,7 +101,7 @@ int main(int arg,char **argv){
       
     }
     Point_2d center = crit_curve[0].caustic_center;
-    PixelMap map(center.x,512*2,1.1*MAX(p2[0]-p1[0],p2[1]-p1[1])/512/2);
+    PixelMap<float> map(center.x,512*2,1.1*MAX(p2[0]-p1[0],p2[1]-p1[1])/512/2);
     
     for(int i=0;i<crit_curve.size();++i){
       //map.AddCurve(crit_curve[i].caustic_curve_outline, i+1);  // this is a outline of the caustic that does not intersect itself
@@ -127,7 +127,7 @@ int main(int arg,char **argv){
     }
     Point_2d center = crit_curve[0].critical_center;
     crit_range = 1.1*MAX(p2[0]-p1[0],p2[1]-p1[1]);
-    PixelMap map(center.x,512,crit_range/512);
+    PixelMap<float> map(center.x,512,crit_range/512);
     
     for(int i=0;i<crit_curve.size();++i)
       map.AddCurve(crit_curve[i].critcurve,i+1);// .critical_curve,i+1);
@@ -141,7 +141,6 @@ int main(int arg,char **argv){
     cout << "Exiting" << endl;
     exit(1);
   }
-  
   //*** print information about the critical curves that were found
   PosType rmax,rmin,rave;
   if(crit_curve.size() > 0){
@@ -162,12 +161,12 @@ int main(int arg,char **argv){
   //*** find a source position within the tangential caustic
   Utilities::RandomNumbers_NR random(seed);   //*** random number generator
   std::vector<Point_2d> y;                    //*** vector for source positions
-  crit_curve[0].RandomSourceWithinCaustic(1,y,random); //*** get random points within first caustic
-  
+  crit_curve[0].RandomSourcesWithinCaustic(1,y,random); //*** get random points within first caustic
+
   PosType zs = 2; //** redshift of source
   //** make a Sersic source, there are a number of other ones that could be used
-  SourceSersic source(23,0.02,0,1,0.5,zs,23);
- 
+  SourceSersic source(23,0.02,0,1,0.5,zs,23,Band::EUC_VIS);
+  
   source.setTheta(y[0]);
   
   /** reset the source plane in the lens from the one given in the
@@ -180,10 +179,10 @@ int main(int arg,char **argv){
 
   std::cout << "Mapping source ..." << std::endl;
   
-  // we could make a PixelMap from the GridMap with  PixelMap map = gridmap.writePixelMap(LensingVariable::SurfBrightness);
-  // but let us re-center the PixelMap on the critical curve
+  // we could make a PixelMap<> from the GridMap with  PixelMap<> map = gridmap.writePixelMap(LensingVariable::SurfBrightness);
+  // but let us re-center the PixelMap<> on the critical curve
   Point_2d crit_center = crit_curve[0].critical_center;
-  PixelMap map(crit_center.x,512,crit_range/512,PixelMapUnits::surfb);
+  PixelMap<float> map(crit_center.x,512,crit_range/512,PixelMapUnits::surfb);
 
   
   // The following produces an image of the lensed source using the rays that
@@ -192,7 +191,7 @@ int main(int arg,char **argv){
   gridmap.RefreshSurfaceBrightnesses(&source);
   map.AddGridMapBrightness(gridmap);
   
-  // You can add a source directly to the PixelMap without lensing with
+  // You can add a source directly to the PixelMap<> without lensing with
   
   source.setTheta(crit_center);
   map.AddSource(source);
