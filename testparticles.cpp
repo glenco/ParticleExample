7581e58938ecba7c1836132087ed3b08b212e9bb
RB Metcalf <bmetcalf@gmail.com>
2025-08-08 15:04:54 +0200

    Removed test lines to make it a better example.

--- testparticles.cpp
+++ testparticles.cpp
@@ -19,60 +19,8 @@ using namespace std;
 int main(int arg,char **argv){
   
   COSMOLOGY cosmo(CosmoParamSet::Planck1yr);
-  Point_3d<float> xo(0,0,0); // center of excised region in comoving Mpc/h
- /** auto lenshalos = LensHaloParticles<float>::MakeLensHaloParticle(
-    "../ClusterCaustics/DataFiles/snap_058"
-    ,SimFileFormat::gadget2
-    ,0.    /// inv_area
-    ,cosmo 
-    ,5.    /// Nsmooth
-    ,8     /// number of buckets in tree
-    ,0.1   /// opening angle for tree
-    ,0     /// rmax for excision
-    ,xo    /// center of excised region in comoving Mpc/h
-    ,true  /// verbose 
-  );
-  */
-  { // tests
-    Utilities::RandomNumbers_NR ran(-28976391);
-    unsigned long nparticles = 100;
-    PosType boundary_p1[3] = {-1000,-1000,-1000};
-    PosType boundary_p2[3] = {1000,1000,1000};
-    //std::vector<double> xp(3*nparticles);
-    std::vector<Point_3d<double> > xp(nparticles);
-    for(auto &p : xp){
-      p[0] = ran()*2000 -1000;
-      p[1] = ran()*1000 -1000;
-      p[2] = ran()*200;
-    }
 
-    OTreeNB<Point_3d<double> > tree(xp.data(),xp.size());
-    tree.build(10);
-
-    Point_2d ray(0,0);
-    double alpha[2],kappa,gamma[3],phi;
-    alpha[0] = alpha[1] = 0.0;
-    gamma[0] = gamma[1] = gamma[2] = 0.0;
-    kappa = phi = 0.0;
-    tree.force2D(  
-      ray.x // ray direction
-      ,1.0e9  // particle mass
-      ,0.  // smooth_factor
-      ,0.1 // theta2
-      ,0. // inv_area
-      ,alpha   // not zeroed
-      ,&kappa // not zeroed
-      ,gamma // not zeroed
-      ,&phi   // not zeroed
-    );
-
-    std::cout << " Total Number of branches " << tree.getTotalBranches() << std::endl;
-    std::cout << " Used branches " << tree.size() << std::endl;
-    std::cout << " Depth " << tree.getDepth() << std::endl;
-  
-  }/**/
-
-   std::string out_dir = "output4/";
+  std::string out_dir = "output4/";
 
   Utilities::LOGPARAMS log_params(out_dir+"params");
   
@@ -98,68 +46,10 @@ int main(int arg,char **argv){
 
   Lens lens(&seed,z_source,cosmo);
 
-  Point_3d<double> center;
-  /*{
-   
-    double range = 15.0 * arcsecTOradians; // range of grids in radians
-    std::vector<RAY> rays(5);
-    for(RAY& r : rays){
-      r.x[0] = range * (random() - 0.5);
-      r.x[1] = range * (random() - 0.5);
-      r.z = z_source; // set the source redshift
-    }
-    
-    Utilities::LOGDATA file("theta_test.csv");
-    time_t t = time(nullptr);
-
-    LensHaloParticles<float> halo("particles.dm.txt"
-                        ,SimFileFormat::ascii
-                        ,zl
-                        ,0. // inverse area
-                        ,3*1.807953375000000000e6 // particle mass
-                        ,cosmo 
-                        ,Nsmooth
-                        ,8  /// number of buckets in tree
-                        ,0.1      /// opening angle for tree
-                        ,true /// re-center on center of mass
-                        );
-
-    Lens lens(&seed,z_source,cosmo);
-    lens.moveinMainHalo(halo, true);
-    std:cout << time(nullptr) - t << " seconds to construct LensHaloParticles" << std::endl;
-    for(float theta = 0 ; theta <= 1 ; theta += 0.01){
-
-      Utilities::LOGDATA::LINE line;
-    
-      //halo.resetForceAngle(theta);
-      line["theta"] = theta;
-
-      // access the main halo and reset the force angle
-      lens.getMainHalo<LensHaloParticles<float> >(0)->resetForceAngle(theta);
-      
-      // this is moved instead of inserted to avoid a copy
-      //center /= cosmo.angDist(zl);
- 
-      // set the redshift of the source plane
-      lens.ResetSourcePlane(z_source,false);
-      lens.rayshooterInternal(rays.size(),rays.data());
-      
-      int i=0;
-      for(auto &r : rays){
-        line["alpha_x"+std::to_string(i)] = r.alpha()[0];
-        line["alpha_y"+std::to_string(i)] = r.alpha()[1];
-        line["kappa"+std::to_string(i)] = r.kappa();
-        line["gamma1"+std::to_string(i)] = r.gamma1();
-        line["gamma2"+std::to_string(i)] = r.gamma2();
-        line["dt"+std::to_string(i)] = r.dt;
-        ++i;
-      }
-      file.add(line);
-    }
-  }
-exit(0);*/
+  Point_2d center(0,0);
   
-    LensHaloParticles<float> halo("particles.dm.txt"
+  // set up a LensHalo from a text file with the particle data
+  LensHaloParticles<float> halo("particles.dm.txt"
                         ,SimFileFormat::ascii
                         ,zl
                         ,0. // inverse area
@@ -172,25 +62,17 @@ exit(0);*/
                         );/**/
     
                                                  
-    //center = halo.CenterOfMass();
+    
     // insert halos into lens
      // this is moved instead of inserted to avoid a copy
     lens.moveinMainHalo(halo, true);
-    
-    // Another way of creating particle halos that is more flexible with
-    // more file types is commented out here.
-//    MakeParticleLenses halomaker("particles.dm.txt",ascii,Nsmooth,true);
-//    halomaker.CreateHalos(cosmo,zl);
-//    //  There is a seporate halo for each type of particle
-//    for(auto h : halomaker.halos){
-//       lens.insertMainHalo(h,zl, true);
-//    }
-//
   
-
-  // here you can rotate each simulation independently
-  //for(int i=0 ; i < lens.getNMainHalos<LensHP>()  ; ++i){
-  //  lens.getMainHalo<LensHP>(i)->rotate(theta);
+  
+  // here you can rotate each simulation about the center of mass
+  //Point_2d theta(PI/2,0);
+  //Point_3d<float> xo = lens.getMainHalo<LensHaloParticles<float> >(i)->CenterOfMass();
+  //for(int i=0 ; i < lens.getNMainHalos<LensHaloParticles<float> >()  ; ++i){
+  //  lens.getMainHalo<LensHaloParticles<float> >(i)->rotate(theta[0],theta[1],xo);
   //}
   
   center /= cosmo.angDist(zl);
@@ -244,8 +126,8 @@ exit(0);*/
   //****************************************
   
   //*** find a source position within the tangential caustic
-  std::vector<Point_2d> y;                    //*** vector for source positions
-  crit_curve[0].RandomSourcesWithinCaustic(1,y,random); //*** get random points within first caustic
+  std::vector<Point_2d> y;                    // vector for source positions
+  crit_curve[0].RandomSourcesWithinCaustic(1,y,random); // get random points within first caustic
 
   PosType zs = 2; //** redshift of source
   //** make a Sersic source, there are a number of other ones that could be used
